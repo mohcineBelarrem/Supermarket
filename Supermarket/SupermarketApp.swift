05175b0cc897d9f5bbb9d2923fb0b3ac7aadbd97
Mohcine Belarrem <belarrem.mohcine@gmail.com>
2024-10-11 21:53:30 +0100

    Initial Commit

---
+++ Supermarket/SupermarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupermarketApp.swift
+//  Supermarket
+//
+//  Created by Mohcine on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct SupermarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SupermarketTests/SupermarketTests.swift
@@ -0,0 +1,17 @@
+//
+//  SupermarketTests.swift
+//  SupermarketTests
+//
+//  Created by Mohcine on 11/10/2024.
+//
+
+import Testing
+@testable import Supermarket
+
+struct SupermarketTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
